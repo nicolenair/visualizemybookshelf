65166ed34e2a2dfd8bad65b26adc09fed7933d6e
Nicole Nair <nicolenair@minerva.kgi.edu>
2022-07-16 20:15:01 +0900

    working search! and add book

--- stacks/urls.py
+++ stacks/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('books-page-num/', views.book_page_num_agg, name='book-list-pagenum'),
     path('books-genre/', views.book_genre_agg, name='book-list-genre'),
     path('books-date/', views.book_date_agg, name='book-list-date'),
-    path('books-search/', views.BookListSearchView.as_view(), name='book-list-search'),
-    path('test-up/', views.test_update, name='test'),
-    path('', RedirectView.as_view(url='books-geo/'), name='index'),
+    path('books-search/', views.book_search_view, name='book-list-search'),
+    path('books-search/book-add/', views.add_book, name='book-add'),
+    path('', RedirectView.as_view(url='books/'), name='index'),
 ]
--- stacks/views.py
+++ stacks/views.py
@@ -16,14 +16,35 @@ class MyBookListView(PermissionRequiredMixin,generic.ListView):
     template_name = 'stacks/book_list.html' 
 
     def get_queryset(self):
-        return Book.objects.filter(users=self.request.user)
+        books = Book.objects.filter(users=self.request.user)
+        return [i.title for i in books]
 
-class MyBookListDateView(PermissionRequiredMixin,generic.ListView):
-    permission_required = 'stacks.can_view_personal_bookshelf'
-    model = Book
-    template_name = 'stacks/book_list_date.html' 
-    def get_queryset(self):
-        return Book.objects.filter(users=self.request.user)
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+def add_book(request):
+    data = request.POST
+    matching_book = Book.objects.get(isbn=data["isbn"])
+    matching_book.users.add(request.user)
+    books = Book.objects.filter(users=request.user)
+    return render(request, 'stacks/book_list.html'  , {"book_list": [i.title for i in books]})
+
+def book_search_view(request):
+    db_update_obj = MaintainBookDatabase()
+    db_update_obj.update_book_info(request.GET.get("searched_title"))
+    matching_book = Book.objects.filter(title=request.GET.get("searched_title"))
+    matching_book = [{"title": i.title, "isbn": i.isbn} for i in matching_book]
+    return render(request, 'stacks/book_list_search_result.html'  , {"book_list": matching_book})
 
 def book_date_agg(request):
     pagenums = Book.objects.filter(users=request.user)
@@ -43,25 +64,6 @@ def book_date_agg(request):
 
     div = opy.plot(figure, auto_open=False, output_type='div')
     return render(request, 'stacks/book_agg_hist.html' , {"graph": div})
-
-class BookListSearchView(generic.ListView):
-    model = Book
-    template_name = 'stacks/book_list_search_result.html' 
-    def get_queryset(self):
-        return Book.objects.filter(title=self.request.GET.get("searched_title"))
-
-def index(request):
-    """View function for home page of site."""
-
-    # Generate counts of some of the main objects
-    num_books = Book.objects.all().count()
-
-    context = {
-        'num_books': num_books,
-    }
-
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
 
 def book_page_num_agg(request):
     pagenums = Book.objects.filter(users=request.user)
@@ -86,7 +88,9 @@ def book_page_num_agg(request):
 def book_genre_agg(request):
     genres = Genre.objects.annotate(num_books=Count(Case(
         When(book__users=request.user, then=1))))
-    trace1 = go.Histogram(y=[p.name for p in genres], x=[p.num_books for p in genres], orientation='h')
+
+    print([p.num_books for p in genres])
+    trace1 = go.Bar(y=[p.name for p in genres], x=[p.num_books for p in genres], orientation='h')
     figure=go.Figure()
     figure.add_trace(trace1)
     figure.update_layout(
@@ -100,14 +104,4 @@ def book_genre_agg(request):
     figure.update_yaxes(tick0=1, dtick=1)
 
     div = opy.plot(figure, auto_open=False, output_type='div')
-    return render(request, 'stacks/book_agg_pagenums.html', {"graph": div})
-
-def test_update(request):
-    db_update_obj = MaintainBookDatabase()
-    db_update_obj()
-    num_books = Book.objects.all().count()
-
-    context = {
-        'num_books': num_books,
-    }
-    return render(request, 'index.html', context=context)
+    return render(request, 'stacks/book_agg_hist.html', {"graph": div})
